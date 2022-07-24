afa5db5cd22eda9764f9e1dccb88b160c8087ad9
Gülsen Keskin <63197899+gulsenkeskin@users.noreply.github.com>
2022-07-24 19:53:17 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# fullstack_movie_recommendation_system
+ TensorFlow 
---
+++ android/app/src/main/kotlin/com/example/fullstack_movie_recommendation_system/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.fullstack_movie_recommendation_system
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
